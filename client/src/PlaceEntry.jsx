30453520eca1a08b5f65652f947457e01a8b1f63
Cleo Chai <itscleochai@gmail.com>
2018-08-03 11:05:41 -0700

    converted styled components back to css modules

--- client/src/PlaceEntry.jsx
+++ client/src/PlaceEntry.jsx
@@ -1,94 +1,95 @@
 import React,{Component} from 'react';
-import styled from 'styled-components';
+import styles from '../dist/style.css';
+// import styled from 'styled-components';
 
-// Styled Components
-const BusinessesLi = styled.li`
-  display: grid;
-  grid-template-columns: 65px 10px 200px;
-  grid-template-rows: 22px 22px 22px 22px;
-`;
+// // Styled Components
+// const BusinessesLi = styled.li`
+//   display: grid;
+//   grid-template-columns: 65px 10px 200px;
+//   grid-template-rows: 22px 22px 22px 22px;
+// `;
 
-const BusinessInfo = styled.div`
-  grid-column: 3;
-  grid-row: 1 / span 4;
-`;
+// const BusinessInfo = styled.div`
+//   grid-column: 3;
+//   grid-row: 1 / span 4;
+// `;
 
-const Thumbnails = styled.img`
-  border-radius: 6px;
-  width: 60px;
-  height: 60px;
-`;
-const InfoStyle1 = styled.span`
-  color: #666;
-  font-size: 75%;
-`;
-const InfoStyle2 = styled.span`
-  color: #333;
-  font-size: 75%;
-`;
-const ReviewsStyle = styled.span`
-  vertical-align: middle;
-`;
-const Links = styled.a`
-  font-size: 90%;
-  &:link {
-    color: #0073bb;
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
-  }
-  &:visited {
-    color: #0073bb;
-    text-decoration: none;
-  }
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-const DotSpacing = styled.span`
-  padding: 0px 4px;
-  color: #999;
-`;
+// const Thumbnails = styled.img`
+//   border-radius: 6px;
+//   width: 60px;
+//   height: 60px;
+// `;
+// const InfoStyle1 = styled.span`
+//   color: #666;
+//   font-size: 75%;
+// `;
+// const InfoStyle2 = styled.span`
+//   color: #333;
+//   font-size: 75%;
+// `;
+// const ReviewsStyle = styled.span`
+//   vertical-align: middle;
+// `;
+// const Links = styled.a`
+//   font-size: 90%;
+//   &:link {
+//     color: #0073bb;
+//     margin: 0;
+//     padding: 0;
+//     text-decoration: none;
+//   }
+//   &:visited {
+//     color: #0073bb;
+//     text-decoration: none;
+//   }
+//   &:hover {
+//     text-decoration: underline;
+//   }
+// `;
+// const DotSpacing = styled.span`
+//   padding: 0px 4px;
+//   color: #999;
+// `;
 
-const StyleStars = styled.span`
-  background: url(https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_design_web/9b34e39ccbeb/assets/img/stars/stars.png);
-  background-size: 132px 560px;
-  display: inline-block;
-  vertical-align: middle;
-  width: 82px;
-  height: 14px;
-  margin: 6px 7px 5px 0px;
-`;
-const Star1 = StyleStars.extend`
-  background-position: 0 -434px;
-`;
-const Star15 = StyleStars.extend`
-  background-position: 0 -448px;
-`;
-const Star2 = StyleStars.extend`
-  background-position: 0 -462px;
-`;
-const Star25 = StyleStars.extend`
-  background-position: 0 -476px;
-`;
-const Star3 = StyleStars.extend`
-  background-position: 0 -490px;
-`;
-const Star35 = StyleStars.extend`
-  background-position: 0 -504px;
-`;
-const Star4 = StyleStars.extend`
-  background-position: 0 -518px;
-`;
-const Star45 = StyleStars.extend`
-  background-position: 0 -532px;
-`;
-const Star5 = StyleStars.extend`
-  background-position: 0 -546px;
-`;
+// const StyleStars = styled.span`
+//   background: url(https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_design_web/9b34e39ccbeb/assets/img/stars/stars.png);
+//   background-size: 132px 560px;
+//   display: inline-block;
+//   vertical-align: middle;
+//   width: 82px;
+//   height: 14px;
+//   margin: 6px 7px 5px 0px;
+// `;
+// const Star1 = StyleStars.extend`
+//   background-position: 0 -434px;
+// `;
+// const Star15 = StyleStars.extend`
+//   background-position: 0 -448px;
+// `;
+// const Star2 = StyleStars.extend`
+//   background-position: 0 -462px;
+// `;
+// const Star25 = StyleStars.extend`
+//   background-position: 0 -476px;
+// `;
+// const Star3 = StyleStars.extend`
+//   background-position: 0 -490px;
+// `;
+// const Star35 = StyleStars.extend`
+//   background-position: 0 -504px;
+// `;
+// const Star4 = StyleStars.extend`
+//   background-position: 0 -518px;
+// `;
+// const Star45 = StyleStars.extend`
+//   background-position: 0 -532px;
+// `;
+// const Star5 = StyleStars.extend`
+//   background-position: 0 -546px;
+// `;
 
-// Component
-class PlaceEntry extends Component {
+
+export default class PlaceEntry extends Component {
   constructor (props) {
     super(props);
     this.state = {
@@ -106,44 +107,59 @@ class PlaceEntry extends Component {
   }
 
   handleRating (rating) {
-    if (rating === 1) {return <Star1 />} 
-    else if (rating === 1.5) {return <Star15 />} 
-    else if (rating === 2) {return <Star2 />} 
-    else if (rating === 2.5) {return <Star25 />} 
-    else if (rating === 3) {return <Star3 />} 
-    else if (rating === 3.5) {return <Star35 />} 
-    else if (rating === 4) {return <Star4 />} 
-    else if (rating === 4.5) {return <Star45 />} 
-    else if (rating === 5) {return <Star5 />}
+    let starRating;
+    if (rating === 1) {starRating = styles.star1}
+    else if (rating === 1.5) {starRating = styles.star15}
+    else if (rating === 2) {starRating = styles.star2}
+    else if (rating === 2.5) {starRating = styles.star25}
+    else if (rating === 3) {starRating = styles.star3}
+    else if (rating === 3.5) {starRating = styles.star35}
+    else if (rating === 4) {starRating = styles.star4}
+    else if (rating === 4.5) {starRating = styles.star45}
+    else if (rating === 5) {starRating = styles.star5}
+    return <span className={[styles.stars, starRating].join(' ')}></span>
+
+    // Styled Components
+    // if (rating === 1) {return <Star1 />} 
+    // else if (rating === 1.5) {return <Star15 />} 
+    // else if (rating === 2) {return <Star2 />} 
+    // else if (rating === 2.5) {return <Star25 />} 
+    // else if (rating === 3) {return <Star3 />} 
+    // else if (rating === 3.5) {return <Star35 />} 
+    // else if (rating === 4) {return <Star4 />} 
+    // else if (rating === 4.5) {return <Star45 />} 
+    // else if (rating === 5) {return <Star5 />}
   }
 
   render () {
     return (
       <div>
-        <BusinessesLi>
-          <Thumbnails src={this.props.place.img} />
-          <BusinessInfo>
-            <strong><Links href="/ADD-LINK-FOR-BUSINESS-IN-DB">
+        <li className={styles.businessLi}>
+          <img className={styles.thumbnail} src={this.props.place.img} />
+          <div className={styles.businessInfo}>
+            <strong><a href="/ADD-LINK-FOR-BUSINESS-IN-DB">
               {this.props.place.name.charAt(0).toUpperCase() + this.props.place.name.slice(1)}
-              </Links>
+              </a>
             </strong>
             <br /> 
-            <InfoStyle1>
-              {this.handleRating(this.props.place.rating)} 
-              <ReviewsStyle>{this.props.place.reviews}  reviews</ReviewsStyle>
-            </InfoStyle1>
+            <span className={styles.info1}>
+              {/* <span className={this.handleRating(this.props.place.rating)}></span> */}
+              {this.handleRating(this.props.place.rating)}
+              <span className={styles.reviews}>{this.props.place.reviews}  reviews</span>
+            </span>
             <br />
-            <InfoStyle2>
-              {this.handlePrice(this.props.place.price)} <DotSpacing>•</DotSpacing> 
+            <span className={styles.info2}>
+              {this.handlePrice(this.props.place.price)} <span className={styles.descDot}>•</span> 
               {this.props.place.mainCategory}, {this.props.place.subCategories}
               <br />
               {this.props.place.city}
-            </InfoStyle2>
-          </BusinessInfo>
-        </BusinessesLi>
+            </span>
+          </div>
+        </li>
       </div>
     )
   }
 }
 
-module.exports = { PlaceEntry, Links };
+// Styled Components - exported Links style component
+// export default { PlaceEntry, Links };
--- client/src/PlaceList.jsx
+++ client/src/PlaceList.jsx
@@ -1,45 +1,48 @@
-import React,{Component} from 'react';
-import { Links, PlaceEntry } from './PlaceEntry.jsx';
+import React,{ Component } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
+import styles from '../dist/style.css';
+// import styled from 'styled-components';
+// import { Links, PlaceEntry } from './PlaceEntry.jsx';
+import PlaceEntry from './PlaceEntry.jsx';
 import { Icon } from 'react-icons-kit';
-import {cutlery} from 'react-icons-kit/fa/cutlery';
-import {ic_local_cafe} from 'react-icons-kit/md/ic_local_cafe';
-import {icecream} from 'react-icons-kit/ionicons/icecream'
-import {ic_local_bar} from 'react-icons-kit/md/ic_local_bar';
-import {ic_local_mall} from 'react-icons-kit/md/ic_local_mall';
-import {hotel} from 'react-icons-kit/fa/hotel';
-import {ic_account_balance} from 'react-icons-kit/md/ic_account_balance';
-import {ic_more_horiz} from 'react-icons-kit/md/ic_more_horiz';
-import {androidSearch} from 'react-icons-kit/ionicons/androidSearch';
-import {ic_today} from 'react-icons-kit/md/ic_today';
+import { cutlery } from 'react-icons-kit/fa/cutlery';
+import { ic_local_cafe } from 'react-icons-kit/md/ic_local_cafe';
+import { icecream } from 'react-icons-kit/ionicons/icecream'
+import { ic_local_bar } from 'react-icons-kit/md/ic_local_bar';
+import { ic_local_mall } from 'react-icons-kit/md/ic_local_mall';
+import { hotel } from 'react-icons-kit/fa/hotel';
+import { ic_account_balance } from 'react-icons-kit/md/ic_account_balance';
+import { ic_more_horiz } from 'react-icons-kit/md/ic_more_horiz';
+import { androidSearch } from 'react-icons-kit/ionicons/androidSearch';
+import { ic_today } from 'react-icons-kit/md/ic_today';
 
-const StyledBodyDiv = styled.div`
-  width: 270px;
-  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-`;
-const StyledH3 = styled.h3`
-  color: #d32323;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 1.3125em;
-  font-family: sans-serif;
-`;
-const StyledUl = styled.ul`
-  margin: 0;
-  padding: 0;
-`;
-const StyledLi = styled.li`
-  margin: 12px 10px 12px 0px;
-  list-style: none;
-  vertical-align: top;
-`;
+// // Styled Components
+// const StyledBodyDiv = styled.div`
+//   width: 270px;
+//   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+// `;
+// const StyledH3 = styled.h3`
+//   color: #d32323;
+//   font-weight: bold;
+//   font-size: 16px;
+//   line-height: 1.3125em;
+//   font-family: sans-serif;
+// `;
+// const StyledUl = styled.ul`
+//   margin: 0;
+//   padding: 0;
+// `;
+// const StyledLi = styled.li`
+//   margin: 12px 10px 12px 0px;
+//   list-style: none;
+//   vertical-align: top;
+// `;
+// const StyledIcon = styled.span`
+//   padding: 0px 13px 0px 5px;
+//   display: inline-flex;
+//   vertical-align: top;
+// `;
 
-const StyledIcon = styled.span`
-  padding: 0px 13px 0px 5px;
-  display: inline-flex;
-  vertical-align: top;
-`;
 export default class PlaceList extends Component {
   constructor(props) {
     super(props);
@@ -82,7 +85,74 @@ export default class PlaceList extends Component {
   render () {
     let { places } = this.state;
     return (
-      <StyledBodyDiv>
+      <div className={styles.main}>
+        <h3>People also viewed</h3>
+        {this.state.places.length > 0 && (
+          <span className={styles.list}>
+            {places.map((place, index) => <PlaceEntry key={index} place={place} />)}
+          </span>
+        )}
+        <h3>Other places nearby</h3>
+        <ul className={styles.list}>
+          <li className={styles.listItem}><a href="/ADD-LINK-FOR-SIMILAR-NEARBY-BUSINESSES">
+            Find more {this.state.mainCategory} Restaurants near {this.state.restaurant}
+          </a></li>
+          <li className={styles.listItem}><a href="/ADD-LINK-FOR-SIMILAR-NEARBY-BUSINESSES">
+            Find more {this.state.subCategories} Restaurants near {this.state.restaurant}
+          </a></li>
+        </ul>
+        <h3>Browse nearby</h3>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={cutlery} /></span>Restaurants</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={ic_local_cafe} /></span>Cafes</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={icecream} /></span>Food</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={ic_local_bar} /></span>Bars</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={ic_local_mall} /></span>Shopping</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={hotel} /></span>Hotels</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={ic_account_balance} /></span>Landmarks</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={ic_more_horiz} /></span>Show all</a>
+          </li>
+        </ul>
+
+        <h3>Dining in {this.state.city}</h3>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+          <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={androidSearch} /></span>
+            Search for reservations</a>
+          </li>
+          <li className={styles.listItem}>
+          <a href="/ADD-LINK"><span className={styles.icon}><Icon size={18} icon={ic_today} /></span>
+            Book a Table in {this.state.city}</a>
+          </li>
+        </ul>
+        <h3>Best of {this.state.city}</h3>
+        <ul className={styles.list}>
+          <li className={styles.listItem}><a href="/ADD-LINK">Things to do in {this.state.city}</a></li>
+        </ul>
+        {/* <button onClick={(e) => {this.createData(e)}}>Click To Create Fake Data</button> */}
+      </div>
+    )
+  }
+}
+
+
+// Styled Components - place inside return()
+{/* <StyledBodyDiv>
         <StyledH3>People also viewed</StyledH3>
         {this.state.places.length > 0 && (
           <StyledUl>
@@ -142,8 +212,5 @@ export default class PlaceList extends Component {
         <StyledUl>
           <StyledLi><Links href="/ADD-LINK">Things to do in {this.state.city}</Links></StyledLi>
         </StyledUl>
-        <button onClick={(e) => {this.createData(e)}}>Click To Create Fake Data</button>
-      </StyledBodyDiv>
-    )
-  }
-}
+          <button onClick={(e) => {this.createData(e)}}>Click To Create Fake Data</button>
+      </StyledBodyDiv> */}
